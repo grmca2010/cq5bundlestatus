64a259b98dbc49c1fe5871c99734b8d549a2e5cb
JM <jopresle@cisco.com>
2015-03-26 09:22:45 -0700

    fixing spacing, styles
    * changing double-quotes to single-quotes.  This style decision allows
    developers to better read each others’ code and avoid quote-type
    related bugs.
    
    * changing tabs to spaces.  Code editors often allow configurable
    widths on tabs, making environment-to-environment visualization
    inconsistent (e.g.: web page diffs)
    
    * changing spacing in function signatures.  Many code editors will
    apply syntax highlighters only when certain spacing conventions are
    met.  Being generous with spacing allows the broadest set of editors
    and syntax highlighters to operate the most effectively.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var cq5bundleapi=require("./lib/main");
+var cq5bundleapi = require('./lib/main'),
+  cq5bundle      = cq5bundleapi.init('http://localhost:4502');
 
-var cq5bundle=cq5bundleapi.init("http://localhost:4502");
-
-cq5bundle.getAllBundleStatus(function(error,response){
-	response.data.forEach(function(element,index,Array){
-		console.log("#Name:" +element.name +"\n  #state:"+element.state);		
-	});
+cq5bundle.getAllBundleStatus(function (error, response) {
+  response.data.forEach(function (element, index, Array) {
+    console.log('#Name: ', element.name, + '#state:', element.state);   
+  });
 });
